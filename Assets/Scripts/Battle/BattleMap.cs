1bae204c101e78e8c0c31af165ce19d1f4d8ca46
Ryusei1115 <tekunikaru22@gmail.com>
2023-01-18 20:21:13 +0900

    WIP

---
+++ Assets/Scripts/Battle/BattleMap.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleMap : MonoBehaviour
+{
+    [SerializeField]
+    [Header("縦のマス数")]
+    private int _height;
+
+    [SerializeField]
+    [Header("横のマス数")]
+    private int _width;
+}
--- Assets/Scripts/Character/Base/CharacterBase.cs
+++ Assets/Scripts/Character/Base/CharacterBase.cs
@@ -2,18 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterBase : MonoBehaviour
+public abstract class CharacterBase : MonoBehaviour
 {
+    public int CurrentHp => _currentHp;
+    public float CurrentSpeed => _currentSpeed;
+    public CharacterType Type => _characterType;
+
     [SerializeField]
     private CharacterType _characterType;
 
     [SerializeField]
-    private int _hp;
+    private int _initialHp;
 
     [SerializeField]
-    private float _speed;
+    private float _initialSpeed;
 
     [SerializeField]
     [Header("出現時の待機時間")]
     private int _delayTime;
+
+    [SerializeField]
+    [Header("出現時の演出")]
+    private ParticleSystem _enableParticle;
+
+    [SerializeField]
+    [Header("死亡時の演出")]
+    private ParticleSystem _deathParticle;
+    
+    protected int _currentHp;
+    protected float _currentSpeed;
+
+    protected Animator _anim;
+
+    private void OnEnable()
+    {
+        _anim ??= GetComponent<Animator>();
+        Init();
+    }
+
+    public abstract void Attack();
+    public abstract void Move();
+
+    protected void Init()
+    {
+        _currentHp = _initialHp;
+        _currentSpeed = _initialSpeed;
+        _deathParticle.gameObject.SetActive(false);
+        _enableParticle.gameObject.SetActive(true);
+    }
+
+    public void RecieveDamage(int damage)
+    {
+        _currentHp -= damage;
+        if(_currentHp <= 0)
+        {
+            Death();
+        }
+    }
+
+    public virtual void Death()
+    {
+        _deathParticle.gameObject.SetActive(true);
+    }
 }
