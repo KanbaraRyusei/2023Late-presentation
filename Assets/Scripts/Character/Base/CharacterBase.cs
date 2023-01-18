c411b5e2ed2b0f0bf9d3eac377a3cc14d6cb6663
Ryusei1115 <tekunikaru22@gmail.com>
2023-01-18 12:21:10 +0900

    WIP

---
+++ Assets/Scripts/Character/Base/CharacterBase.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBase : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterType _characterType;
+
+    [SerializeField]
+    private int _hp;
+
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    [Header("oŒ»‚Ì‘Ò‹@ŠÔ")]
+    private int _delayTime;
+}
---
+++ Assets/Scripts/Character/Enum/CharacterType.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CharacterType
+{
+    Spirit,
+    Spell,
+    Building,
+    Monster,
+    Player
+}
